c0e5d91609d09aa63226d9bc57a10409023e8d56
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-31 13:36:53 -0800

    Update guessing-game.js

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,17 @@
 function guessingGame() {
-
+	const randNum = Math.floor(Math.random() * 100);
+  let tries = 0;
+  let won = false;
+  const game = function(guess){
+  	if(won) return "The game is over, you already won!";
+    
+  	tries++;
+  	if(guess < randNum) return guess + " is too low!";
+    if(guess > randNum) return guess + " is too high!";
+    if(guess === randNum) return "You win! You found " + randNum + " in " + tries + " guesses.";
+  };
+  
+  return game;
 }
 
 module.exports = { guessingGame };
