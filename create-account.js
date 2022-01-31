b1105003c9c144245c8eb7b2a7d49baaa9bfca70
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-31 13:50:33 -0800

    Update create-account.js

--- create-account.js
+++ create-account.js
@@ -1,5 +1,31 @@
 function createAccount(pin, amount) {
+	//let balance = amount;
 
+	function checkBalance(entry){
+  	if(entry === pin) return amount;
+    
+    return 'Invalid PIN.';
+  }
+  
+  function deposit(entry, deposit){
+  	if(entry !== pin) return 'Invalid PIN.';
+    
+    amount+=deposit;
+  }
+  
+  function withdraw(entry, withdrawl){
+  	if(entry !== pin) return 'Invalid PIN.';
+    
+    amount-=withdrawl;
+  }
+  
+  function changePin(oldPin, newPin){
+  	if(oldPin !== pin) return 'Invalid PIN.';
+    
+    pin = newPin;
+  }
+  
+  return {checkBalance, deposit, withdraw, changePin};
 }
 
 module.exports = { createAccount };
