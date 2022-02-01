5b85961cbd99c561a12b6d9de89b025dfc31f135
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-02-01 09:53:17 -0800

    Update curried-add.js

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,16 @@
 function curriedAdd(total) {
-
+  function ret(input){
+  	if(input === undefined) return total;
+    
+    total+=input;
+    return ret;
+  }
+  
+  if(total === undefined){
+    return 0;
+  }
+  
+  return ret;
 }
 
 module.exports = { curriedAdd };
